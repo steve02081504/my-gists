ad27bd4ff562b1595370c3983bc9085412a625de
steve02081504 <steve02081504@EDEN.fukwold>
2021-09-19 17:46:43 +0800

    Update shiori_loader.hpp

--- ukagaka/shiori_loader.hpp
+++ ukagaka/shiori_loader.hpp
@@ -1,22 +1,29 @@
 struct Cshiori{
 	HINSTANCE dll=NULL;
 
-	typedef BOOL __cdecl (*load_type)(HGLOBAL h, long len);
-	typedef BOOL __cdecl (*unload_type)();
-	typedef HGLOBAL __cdecl (*request_type)(HGLOBAL h, long *len)
+	typedef BOOL __cdecl load_type_t(HGLOBAL h, long len);
+	typedef load_type_t* load_type;
+	typedef BOOL __cdecl unload_type_t();
+	typedef unload_type_t* unload_type;
+	typedef HGLOBAL __cdecl request_type_t(HGLOBAL h, long* len);
+	typedef request_type_t* request_type;
 
 	load_type load=NULL;
 	unload_type unload=NULL;
 	request_type request=NULL;
 
-	init_methods(){
-		load=(load_type)GetProcAddress(dll,L"load");
-		unload=(unload_type)GetProcAddress(dll,L"unload");
-		request=(request_type)GetProcAddress(dll,L"request");
+	void init_methods(){
+		load=(load_type)GetProcAddress(dll,"load");
+		unload=(unload_type)GetProcAddress(dll,"unload");
+		request=(request_type)GetProcAddress(dll,"request");
 	}
-	Cshiori(LPCWSTR pszFileName){
+	Cshiori(){}
+	void SetTo(LPCWSTR pszFileName){
 		dll=LoadLibrary(pszFileName);
 		init_methods();
+	}
+	Cshiori(LPCWSTR pszFileName){
+		SetTo(pszFileName);
 	}
 	~Cshiori(){
 		FreeLibrary(dll);
