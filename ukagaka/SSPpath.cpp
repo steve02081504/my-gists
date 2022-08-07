c9f3ca812581787408ae8d67d8276523f6b122f6
steve02081504 <steve02081504@EDEN.fukwold>
2022-08-08 00:46:51 +0800

    Create SSPpath.cpp

---
+++ ukagaka/SSPpath.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <Windows.h>
+
+std::wstring GetSSPpath() {
+	// Get the path to the SSP folder at reg HKEY_CLASSES_ROOT\x-ukagaka-link\shell\open\command
+	HKEY hKey;
+	LONG lResult = RegOpenKeyExW(HKEY_CLASSES_ROOT, L"x-ukagaka-link\\shell\\open\\command", 0, KEY_READ, &hKey);
+	if(lResult != ERROR_SUCCESS) {
+		return L"";
+	}
+	DWORD	dwType;
+	DWORD	dwSize = MAX_PATH;
+	wchar_t szValue[MAX_PATH];
+	lResult = RegQueryValueExW(hKey, NULL, NULL, &dwType, (LPBYTE)szValue, &dwSize);
+	RegCloseKey(hKey);
+	if(lResult != ERROR_SUCCESS) {
+		return L"";
+	}
+	//szValue like "E:\ssp\ssp.exe" /M "%1"
+	std::wstring sspPath = szValue;
+	auto		 pos	 = sspPath.find_last_of(L"\" /M \"%1\"");
+	if(pos == std::wstring::npos) {
+		return L"";
+	}
+	sspPath = sspPath.substr(1, pos - 9);
+	return sspPath;
+}
+
+bool IsSSPinstalled() {
+	std::wstring sspPath = GetSSPpath();
+	if(sspPath.empty()) {
+		return false;
+	}
+	return _waccess(sspPath.c_str(), 0) == 0;
+}
---
+++ ukagaka/SSPpath.hpp
@@ -0,0 +1,4 @@
+#include <string>
+
+[[nodiscard]] std::wstring GetSSPpath();
+[[nodiscard]] bool IsSSPinstalled();
