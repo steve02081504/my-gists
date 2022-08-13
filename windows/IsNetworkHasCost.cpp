a4b2baa5b40fa07f084ea37b8e7eda398a5872ea
steve02081504 <steve02081504@EDEN.fukwold>
2022-08-13 20:24:37 +0800

    fix

--- windows/IsNetworkHasCost.cpp
+++ windows/IsNetworkHasCost.cpp
@@ -3,7 +3,8 @@ bool IsNetworkHasCost() {
 	winrt::init_apartment();
 	auto internetConnectionProfile{winrt::Windows::Networking::Connectivity::NetworkInformation::GetInternetConnectionProfile()};
 	auto ConnectionCost = internetConnectionProfile.GetConnectionCost();
-	if(ConnectionCost.NetworkCostType() != winrt::Windows::Networking::Connectivity::NetworkCostType::Unrestricted)
+	auto NetworkCostType = ConnectionCost.NetworkCostType();
+	if(NetworkCostType == winrt::Windows::Networking::Connectivity::NetworkCostType::Unrestricted)
 		return false;
 	else
 		return true;
