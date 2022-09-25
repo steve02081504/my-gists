e8f10fee4abd3ffdc2263e3dc4a872335eded669
steve02081504 <steve02081504@EDEN.fukwold>
2022-09-25 11:25:56 +0800

    Update IsNetworkHasCost.cpp

--- windows/IsNetworkHasCost.cpp
+++ windows/IsNetworkHasCost.cpp
@@ -1,5 +1,8 @@
 #include <winrt/Windows.Networking.Connectivity.h>
+#include "versionhelpers.h"
 bool IsNetworkHasCost() {
+	if(!IsWindows8OrGreater())
+		return false;
 	winrt::init_apartment();
 	auto internetConnectionProfile{winrt::Windows::Networking::Connectivity::NetworkInformation::GetInternetConnectionProfile()};
 	auto ConnectionCost	 = internetConnectionProfile.GetConnectionCost();
