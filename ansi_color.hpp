475543e93f1ab6140f2b0ca5a6ddcc9e1bc5d0eb
steve02081504 <steve02081504@EDEN.fukwold>
2023-02-26 14:37:42 +0800

    Create ansi_color.hpp

---
+++ ansi_color.hpp
@@ -0,0 +1,12 @@
+#define SET_GREEN		 "\033[32m"
+#define SET_RED			 "\033[31m"
+#define SET_LIGHT_YELLOW "\033[93m"
+#define RESET_COLOR		 "\033[0m"
+
+#define GREEN_TEXT(text)		SET_GREEN text RESET_COLOR
+#define RED_TEXT(text)			SET_RED text RESET_COLOR
+#define LIGHT_YELLOW_TEXT(text) SET_LIGHT_YELLOW text RESET_COLOR
+
+#define GREEN_OUTPUT(something)		   SET_GREEN << something << RESET_COLOR
+#define RED_OUTPUT(something)		   SET_RED << something << RESET_COLOR
+#define LIGHT_YELLOW_OUTPUT(something) SET_LIGHT_YELLOW << something << RESET_COLOR
